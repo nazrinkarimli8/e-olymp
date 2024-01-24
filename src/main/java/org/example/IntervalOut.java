0a5f49551ec36b4f87a2eb21e5af9fe545e52756
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-01-24 15:19:01 +0400

    First commit

---
+++ src/main/java/org/example/IntervalOut.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class IntervalOut {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long x = sc.nextInt();
+        long a = sc.nextInt();
+        long b = sc.nextInt();
+        if (x < a || x > b) {
+            System.out.println("OUT");
+        } else {
+            System.out.println("IN");
+        }
+    }
+}
