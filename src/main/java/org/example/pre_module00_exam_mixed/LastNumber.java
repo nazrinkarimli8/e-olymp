91454f69a5843dd11810ca87042423ff6c28b9cf
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-02-09 12:34:28 +0400

    fix

---
+++ src/main/java/org/example/pre_module00_exam_mixed/LastNumber.java
@@ -0,0 +1,11 @@
+package org.example.pre_module00_exam_mixed;
+
+import java.util.Scanner;
+
+public class LastNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n % 10);
+    }
+}
