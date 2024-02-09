2d173ce84c1f02c330c838985790950358d3f29d
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-02-09 21:55:05 +0400

    fix

--- src/main/java/org/example/pre_module00_exam_mixed/RatioOfSum.java
+++ src/main/java/org/example/pre_module00_exam_mixed/RatioOfSum.java
@@ -2,7 +2,7 @@ package org.example.pre_module00_exam_mixed;
 
 import java.util.Scanner;
 
-public class DivisionOfSum {
+public class RatioOfSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long k = sc.nextLong();
