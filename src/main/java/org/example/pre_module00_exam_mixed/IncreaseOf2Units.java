25a006e0f9f05b9178c47403922fb899dfbecfaf
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-02-12 15:43:53 +0400

    fix

---
+++ src/main/java/org/example/pre_module00_exam_mixed/IncreaseOf2Units.java
@@ -0,0 +1,23 @@
+package org.example.pre_module00_exam_mixed;
+
+import java.util.Scanner;
+
+public class IncreaseOf2Units {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= 0) {
+                arr[i] = arr[i] + 2;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
