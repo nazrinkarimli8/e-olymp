4951631d9d3b451d6923bf176a1b8cfa4f9c3547
nazrinkarimli8 <karimlinazrin8@gmail.com>
2024-02-12 14:27:48 +0400

    fix

---
+++ src/main/java/org/example/pre_module00_exam_mixed/SumOfAandB.java
@@ -0,0 +1,14 @@
+package org.example.pre_module00_exam_mixed;
+
+import java.util.Scanner;
+
+public class SumOfAandB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
